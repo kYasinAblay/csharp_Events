53a5bdacd0e37464bb1685236b6ca9c16d479ffc
Kamil Yasin ABLAY <KamilyasinAblay@hotmail.com>
2022-07-31 18:21:38 +0300

    Add project files.

---
+++ csharp_Events/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+        bl.StartProcess();
+        Console.ReadKey();
+
+
+        // event handler
+        static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!");
+        }
+
+public delegate void Notify();  // delegate
+
+public class ProcessBusinessLogic
+{
+    public event Notify ProcessCompleted; // event
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        // some code here..
+        OnProcessCompleted();
+    }
+
+    protected virtual void OnProcessCompleted() //protected virtual method
+    {
+        //if ProcessCompleted is not null then call delegate
+        ProcessCompleted?.Invoke();
+    }
+}
