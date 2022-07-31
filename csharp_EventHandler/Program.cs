2c8fd48bf2a65353333e3f2279782e192a78ca38
Kamil Yasin ABLAY <KamilyasinAblay@hotmail.com>
2022-07-31 18:25:00 +0300

    okey

---
+++ csharp_EventHandler/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+				
+
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+        bl.StartProcess();
+    
+
+    // event handler
+     static void bl_ProcessCompleted(object sender, EventArgs e)
+    {
+        Console.WriteLine("Process Completed!");
+    }
+
+
+public class ProcessBusinessLogic
+{
+    public event EventHandler ProcessCompleted; // event
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        // some code here..
+        OnProcessCompleted(EventArgs.Empty);
+    }
+
+
+    protected virtual void OnProcessCompleted(EventArgs e)
+    {
+        ProcessCompleted?.Invoke(this, e);
+    }
+}
